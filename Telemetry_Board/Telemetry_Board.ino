f4fa57fc4037c49ee903050f8af7ee857ac3a6ac
Tristan <trisdooley@gmail.com>
2025-04-22 13:03:55 -0600

    uart test

--- Telemetry_Board/Telemetry_Board.ino
+++ Telemetry_Board/Telemetry_Board.ino
@@ -3,14 +3,28 @@
 
 HardwareSerial SerialPort(2); // use UART2
 
+unsigned long lastTime = 0;
+int dataToSend = 0;
 
 void setup() {
-  SerialPort.begin(57600, SERIAL_8N1, SCL, SDA); 
+  Serial.begin(115200);
+  SerialPort.begin(57600, SERIAL_8N1, PIN5, PIN6); 
 } 
 
+void sendData() {
+  if(millis() - lastTime > 500) {
+    SerialPort.print(dataToSend);
+    Serial.println(dataToSend);
+    dataToSend++;
+    dataToSend = dataToSend%2;
+    lastTime = millis();
+  }
+}
+
 void loop() { 
-  SerialPort.print(1);
-  delay(500);
-  SerialPort.print(0);
-  delay(500);
+  sendData();
+  while(SerialPort.available() > 0) {
+    Serial.print(SerialPort.read());
+  }
+  delay(1);
 }
